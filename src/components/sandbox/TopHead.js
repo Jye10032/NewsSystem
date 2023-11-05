47f13e3ea24cdf7fbb10aa1afd277c72540c15f6
Jye10032 <736891807@qq.com>
2023-11-05 14:42:51 +0800

    在v5基础上进行了修改

--- src/components/sandbox/TopHead.js
+++ src/components/sandbox/TopHead.js
@@ -2,42 +2,40 @@ import React, { useState } from 'react'
 import { Layout, Button, theme, Dropdown } from 'antd'
 import { MenuUnfoldOutlined, MenuFoldOutlined, SmileOutlined, DownOutlined, UserOutlined } from '@ant-design/icons'
 import { Avatar, Space } from 'antd'
-const { Header } = Layout
-const items = [
-    {
-        key: '1',
-        label: (
-            <a target="_blank" rel="noopener noreferrer" href="https://www.antgroup.com">
-                1st menu item
-            </a>
-        ),
-    },
-    {
-        key: '2',
-        label: (
-            <a target="_blank" rel="noopener noreferrer" href="https://www.aliyun.com">
-                2nd menu item (disabled)
-            </a>
-        ),
-        icon: <SmileOutlined />,
-        disabled: true,
-    },
-    {
-        key: '3',
-        label: (
-            <a target="_blank" rel="noopener noreferrer" href="https://www.luohanacademy.com">
-                3rd menu item (disabled)
-            </a>
-        ),
-        disabled: true,
-    },
-    {
-        key: '4',
-        danger: true,
-        label: '退出',
-    },
-];
+import { useNavigate } from 'react-router-dom'
+
+
+
 export default function TopHead() {
+
+
+    const { Header } = Layout
+    const navigate = useNavigate()
+
+
+    // 退出登录
+    function logout() {
+        localStorage.removeItem('token')
+        navigate('/login', { replace: true })
+    }
+
+    const items = [
+        {
+            key: '1',
+            label: (
+                <a target="_blank" rel="noopener noreferrer" href="https://www.antgroup.com">
+                    超级管理员
+                </a>
+            ),
+        },
+        {
+            key: '4',
+            danger: true,
+            label: '退出',
+            onClick: logout
+        },
+    ];
+
     const [collapsed, setCollapsed] = useState(false);
     const {
         token: { colorBgContainer },
