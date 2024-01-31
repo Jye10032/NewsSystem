d3f08391068ab9bd8949fd19c8ea8ed5dad23a8a
Jye10032 <736891807@qq.com>
2024-01-31 22:35:00 +0800

    修改了路由和部分warning

--- src/components/sandbox/SideMenu.js
+++ src/components/sandbox/SideMenu.js
@@ -3,9 +3,14 @@ import { Layout, Menu, theme } from 'antd'
 import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
 import { useNavigate, useLocation } from 'react-router-dom';
 import {
+    HomeOutlined,
     UserOutlined,
+    CrownOutlined,
+    ContainerOutlined,
+    AuditOutlined,
+    ExceptionOutlined,
     VideoCameraOutlined,
-    UploadOutlined,
+    UploadOutlined
 } from '@ant-design/icons'
 import './index.css'
 import axios from 'axios';
@@ -18,56 +23,56 @@ const { Sider } = Layout
 //     getItem('用户管理', '/user-manage', <VideoCameraOutlined />, [getItem('用户列表', 'user/manege/list')]),
 //     getItem('权限管理', '/right-manage', <UploadOutlined />, [getItem('角色列表', '/right-manege/role/list'), getItem('权限列表', '/right-manege/right/list')]),
 // ];
-const menuList = [
-    {
-        key: '/home',
-        title: '首页',
-        icon: <UserOutlined />,
-    },
-    {
-        key: '/user-manage',
-        title: '用户管理',
-        icon: <VideoCameraOutlined />,
-        children: [
-            {
-                key: '/user-manage/list',
-                title: '用户列表',
-                icon: <UserOutlined />,
-            }
-        ]
-    },
-    {
-        key: '/right-manage',
-        title: '权限管理',
-        icon: <UploadOutlined />,
-        children: [
-            {
-                key: '/right-manage/role/list',
-                title: '角色列表',
-                icon: <UserOutlined />,
-            },
-            {
-                key: '/right-manage/right/list',
-                title: '权限列表',
-                icon: <UserOutlined />,
+// const menuList = [
+//     {
+//         key: '/home',
+//         title: '首页',
+//         icon: <UserOutlined />,
+//     },
+//     {
+//         key: '/user-manage',
+//         title: '用户管理',
+//         icon: <VideoCameraOutlined />,
+//         children: [
+//             {
+//                 key: '/user-manage/list',
+//                 title: '用户列表',
+//                 icon: <UserOutlined />,
+//             }
+//         ]
+//     },
+//     {
+//         key: '/right-manage',
+//         title: '权限管理',
+//         icon: <UploadOutlined />,
+//         children: [
+//             {
+//                 key: '/right-manage/role/list',
+//                 title: '角色列表',
+//                 icon: <UserOutlined />,
+//             },
+//             {
+//                 key: '/right-manage/right/list',
+//                 title: '权限列表',
+//                 icon: <UserOutlined />,
 
-            }
-        ]
-    },
-];
+//             }
+//         ]
+//     },
+// ];
 const iconList = {
-    "/home": <UserOutlined />,
-    "/user-manage/list": <VideoCameraOutlined />,
-    "/right-manage": <UploadOutlined />,
-    "/right-manage/role/list": <UserOutlined />,
-    "/right-manage/right/list": <UserOutlined />,
-    //...
+    '/home': <HomeOutlined />,
+    '/user-manage': <UserOutlined />,
+    '/right-manage': <CrownOutlined />,
+    '/news-manage': <ContainerOutlined />,
+    '/audit-manage': <AuditOutlined />,
+    '/publish-manage': <ExceptionOutlined />
 }
 export default function SideMenu() {
 
     const [meun, setMeun] = useState([])
     useEffect(() => {
-        axios.get("http://localhost:8000/rights?_embed=children").then(res => {
+        axios.get("/rights?_embed=children").then(res => {
             console.log(res.data)
             setMeun(res.data)
         })
@@ -76,8 +81,8 @@ export default function SideMenu() {
     const checkPagePermission = (item) => {
         return item.pagepermisson === 1
     }
-    const renderMenu = (menuList) => {
-        return menuList.map(item => {
+    const renderMenu = (meun) => {
+        return meun.map(item => {
             if (item.children?.length > 0 && checkPagePermission(item)) {
                 return (
                     <SubMenu key={item.key} icon={iconList[item.key]} title={item.title}>
@@ -146,3 +151,93 @@ export default function SideMenu() {
 //         type,
 //     };
 // }
+// import './SideMenu.scss'
+// import { connect } from 'react-redux'
+// import { withRouter } from 'react-router-dom'
+// import React, { useEffect, useState } from 'react'
+// import { Menu, Layout } from 'antd'
+// import {
+//     HomeOutlined,
+//     UserOutlined,
+//     CrownOutlined,
+//     ContainerOutlined,
+//     AuditOutlined,
+//     ExceptionOutlined
+// } from '@ant-design/icons'
+// import axios from 'axios'
+// const { Sider } = Layout
+
+// function SideMenu(props) {
+//     const [items, setItems] = useState([])
+//     const iconList = {
+//         '/home': <HomeOutlined />,
+//         '/user-manage': <UserOutlined />,
+//         '/right-manage': <CrownOutlined />,
+//         '/news-manage': <ContainerOutlined />,
+//         '/audit-manage': <AuditOutlined />,
+//         '/publish-manage': <ExceptionOutlined />
+//     }
+//     // 获取用户可查看到的网页
+//     useEffect(() => {
+//         axios.get('/rights?_embed=children').then((res) => {
+//             const list = res.data
+//             list.forEach((item) => {
+//                 if (item.children.length === 0) item.children = ''
+//             })
+//             setItems(res.data)
+//         })
+//     }, [])
+//     function obj(key, title, icon, children) {
+//         return {
+//             key,
+//             label: title,
+//             icon,
+//             children
+//         }
+//     }
+//     // 递归来获得items数组，该数据用为侧边栏数据
+//     function dfs(list) {
+//         const {
+//             role: { rights }
+//         } = JSON.parse(localStorage.getItem('token'))
+//         const arr = []
+//         list.map((item) => {
+//             if (item.children && item.children.length !== 0 && rights.includes(item.key)) {
+//                 return arr.push(obj(item.key, item.title, iconList[item.key], dfs(item.children)))
+//             } else {
+//                 return (
+//                     item.pagepermisson && rights.includes(item.key) && arr.push(obj(item.key, item.title, iconList[item.key]))
+//                 )
+//             }
+//         })
+//         return arr
+//     }
+//     // 初识选中的菜单项
+//     const selectedKeys = [props.location.pathname]
+//     // 初始展开的子菜单项
+//     const openKeys = ['/' + props.location.pathname.split('/')[1]]
+//     return (
+//         <Sider
+//             trigger={null}
+//             collapsible
+//             collapsed={props.isCollapsible}
+//         >
+//             <div style={{ display: 'flex', height: '100%', flexDirection: 'column' }}>
+//                 <div className="logo">{!props.isCollapsible && <span>全球新闻发布管理系统</span>}</div>
+//                 <Menu
+//                     style={{ overflow: 'auto', flex: '1' }}
+//                     theme="dark"
+//                     mode="inline"
+//                     defaultSelectedKeys={selectedKeys}
+//                     defaultOpenKeys={openKeys}
+//                     items={dfs(items)}
+//                     onClick={(e) => props.history.push(e.key)}
+//                 />
+//             </div>
+//         </Sider>
+//     )
+// }
+
+// export default connect((state) => ({
+//     isCollapsible: state.collapsible
+// }))(withRouter(SideMenu))
